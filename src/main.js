c86497642e6c4ec2ebd793a0374fbebbae0b618e
wander <w.grevink@sig.eu>
2021-10-15 23:28:15 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -1,21 +1,7 @@
 import App from './App.svelte'
-import Table from './Table.svelte'
-import Gallery from './Gallery.svelte'
 
-const menuDiv = document.querySelector('#menu')
-menuDiv &&
+const appDiv = document.querySelector('#app')
+appDiv &&
   new App({
-    target: menuDiv
-  })
-
-const viewDiv = document.querySelector('#view')
-viewDiv &&
-  new Table({
-    target: viewDiv
-  })
-
-const galleryDiv = document.querySelector('#gallery')
-galleryDiv &&
-  new Gallery({
-    target: galleryDiv
+    target: appDiv
   })
