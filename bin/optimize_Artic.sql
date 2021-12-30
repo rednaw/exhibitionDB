956bd91f6895bb428b1de49d073e5d6aed8f1700
wander <w.grevink@sig.eu>
2021-12-30 22:48:46 +0100

    don't use IIF

--- bin/optimize_Artic.sql
+++ bin/optimize_Artic.sql
@@ -13,12 +13,14 @@ INSERT INTO tmpTable(id, title, start_at, end_at, image, catalogue, description)
     title, 
     substr(aic_start_at, 1, 10), 
     substr(aic_end_at, 1, 10), 
-    iif(image_id == '', 'no', 'yes'),
-    iif(length(document_ids) == 2, 'no', 'yes'),
-    iif(description == '', 'no', 'yes')
+    CASE WHEN image_id == '' THEN 'no' ELSE 'yes' END,
+    CASE WHEN length(document_ids) == 2 THEN 'no' ELSE 'yes' END,
+    CASE WHEN description == '' THEN 'no' ELSE 'yes' END
   FROM exhibitions;
 DROP TABLE exhibitions;
 ALTER TABLE tmpTable RENAME TO exhibitions; 
+
+
 
 DELETE FROM artworks WHERE image_id = '';
 CREATE TABLE IF NOT EXISTS tmpTable(id TEXT, title TEXT, artist TEXT, type TEXT, date TEXT, image_id TEXT);
