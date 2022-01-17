55c05a33a25dd97fe7f94ba9a4bf5d0ffd3010e1
wander <w.grevink@sig.eu>
2022-01-17 15:57:38 +0100

    fix

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,9 +1,9 @@
 import { derived } from 'svelte/store'
 import { dictionary, locale, _ } from 'svelte-i18n'
 
-let cachedLocale
+export { _, locale }
 
-async function setupI18n(_locale) {
+export async function setupI18n(_locale) {
   const message_file = `static/lang/${_locale}.json`
   const response = await fetch(message_file)
   if (!response.ok) {
@@ -11,10 +11,8 @@ async function setupI18n(_locale) {
   }
   const messages = await response.json()
   dictionary.set({ [_locale]: messages })
-  cachedLocale = _locale
   locale.set(_locale)
 }
 
-const isLocaleLoaded = derived(locale, $locale => typeof $locale === 'string')
+export const isLocaleLoaded = derived(locale, $locale => typeof $locale === 'string')
 
-export { _, locale, setupI18n, isLocaleLoaded }
